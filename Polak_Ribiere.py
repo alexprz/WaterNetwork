52cac44784833f3e094c03ee3e3d4e5fdd624152
Unknown <alexperez@wanadoo.fr>
2019-04-24 12:39:16 +0200

    Semi working code

--- Polak_Ribiere.py
+++ Polak_Ribiere.py
@@ -47,7 +47,7 @@ def Polak_Ribiere(Oracle, x0):
         alpha_0=1#-2*delta_k/(np.vdot(gradient_n, D))
 
         alpha_p = alpha_n
-        alpha_n, ok = Wolfe(alpha_0, x, D, Oracle, check_direction=True)
+        alpha_n, ok = Wolfe(alpha_0, x, D, Oracle, check_direction=False)
         #
         # print("alpha", alpha_n)
         # print("ok", ok)
